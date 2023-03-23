a29dad000ab0bed3a71b7ebd5f7c65207a564562
rohinsha <r2hinsha@gmail.com>
2023-03-23 18:28:24 +0530

    Add context.consumer in comp2

--- src/Comp2.js
+++ src/Comp2.js
@@ -1,15 +1,21 @@
 import React, { useContext } from "react";
 import LanguageContext from "./language-context";
 const Comp2 = () => {
-  const { language, setLanguage } = useContext(LanguageContext);
+  //const { language, setLanguage } = useContext(LanguageContext);
   return (
-    <div style={{ border: "1px solid black", margin: "20px" }}>
-      <h3>Child2 - {language}</h3>
+    <LanguageContext.Consumer>
+      {({ language, setLanguage }) => {
+        return (
+          <div style={{ border: "1px solid black", margin: "20px" }}>
+            <h3>Child2 - {language}</h3>
 
-      <button onClick={() => setLanguage("Comp2 lang")}>
-        Click on Comp2 ({language})
-      </button>
-    </div>
+            <button onClick={() => setLanguage("Comp2 lang")}>
+              Click on Comp2 ({language})
+            </button>
+          </div>
+        );
+      }}
+    </LanguageContext.Consumer>
   );
 };
 export default Comp2;
